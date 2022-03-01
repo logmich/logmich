23cff5642f96a8dd095e2faab1e7821f542e315e
Ingo Ruhnke <grumbel@gmail.com>
2022-03-01 11:49:53 +0100

    Remove superfluous nix input from Nix flake

--- flake.nix
+++ flake.nix
@@ -2,30 +2,26 @@
   description = "Software Surface Library";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    nix.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "gitlab:grumbel/cmake-modules";
-    tinycmmc.inputs.nix.follows = "nix";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
     tinycmmc.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nix, nixpkgs, flake-utils, tinycmmc }:
+  outputs = { self, nixpkgs, flake-utils, tinycmmc }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in rec {
         packages = flake-utils.lib.flattenTree {
-          wstsound = pkgs.stdenv.mkDerivation {
+          logmich = pkgs.stdenv.mkDerivation {
             pname = "logmich";
             version = "0.2.0";
             src = nixpkgs.lib.cleanSource ./.;
             nativeBuildInputs = [
               pkgs.cmake
-              pkgs.ninja
-              pkgs.gcc
             ];
             buildInputs = [
               tinycmmc.defaultPackage.${system}
@@ -35,6 +31,6 @@
             ];
            };
         };
-        defaultPackage = packages.wstsound;
+        defaultPackage = packages.logmich;
       });
 }
