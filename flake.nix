33a5e05e9fb4cea1fc92346ccfa0ab739d906f02
Ingo Ruhnke <grumbel@gmail.com>
2022-08-20 16:33:59 +0200

    Remove unused flake-utils dependency

--- flake.nix
+++ flake.nix
@@ -3,17 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
     tinycmmc.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, tinycmmc }:
+  outputs = { self, nixpkgs, tinycmmc }:
     tinycmmc.lib.eachSystemWithPkgs (pkgs:
       {
-        packages = flake-utils.lib.flattenTree rec {
+        packages = rec {
           default = logmich;
           logmich = pkgs.callPackage ./logmich.nix {
             tinycmmc = tinycmmc.packages.${pkgs.system}.default;
