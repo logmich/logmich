288566056aab08c428a4dcf8adc24e41ebd6ff86
Ingo Ruhnke <grumbel@gmail.com>
2022-09-20 16:20:32 +0200

    Update defaultPackage to packages.${system}.default

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "Software Surface Library";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
@@ -14,23 +12,29 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages = flake-utils.lib.flattenTree {
+      in {
+        packages = rec {
+          default = logmich;
+
           logmich = pkgs.stdenv.mkDerivation {
             pname = "logmich";
             version = "0.1.x-" + (if (self ? shortRev) then self.shortRev else "dirty");
+
             src = nixpkgs.lib.cleanSource ./.;
+
             nativeBuildInputs = [
               pkgs.cmake
             ];
+
             buildInputs = [
-              tinycmmc.defaultPackage.${system}
+              tinycmmc.packages.${system}.default
             ];
+
             propagatedBuildInputs = [
               pkgs.boost
             ];
            };
         };
-        defaultPackage = packages.logmich;
-      });
+      }
+    );
 }
